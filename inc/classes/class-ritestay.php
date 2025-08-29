840746722192675a1bd543a1ccfd299c05776f75
BhavinPadiya <143876932+BhavinPadiya@users.noreply.github.com>
2025-08-29 17:50:01 +0530

    feat: created dynamic logo uploader from admin panel

--- inc/classes/class-ritestay.php
+++ inc/classes/class-ritestay.php
@@ -17,7 +17,7 @@ class RITESTAY
     protected function __construct()
     {
         //load classes
-        Assets::get_instance(); 
+        Assets::get_instance();
         $this->setup_hooks();
     }
 
@@ -25,10 +25,19 @@ class RITESTAY
     {
         //actions and filters
         add_action('after_setup_theme', [$this, 'setup_theme']);
+        
     }
 
-    public function setup_theme(){
+    public function setup_theme()
+    {
         add_theme_support('title-tag');
-    }
 
+	add_theme_support( 'custom-logo', [
+        'header-text'          => array( 'site-title', 'site-description' ),
+        'height'               => 100,
+		'width'                => 400,
+		'flex-height'          => true,
+		'flex-width'           => true,
+    ]);
+    }
 }
