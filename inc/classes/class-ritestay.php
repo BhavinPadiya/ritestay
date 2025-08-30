751e0f6c20d7e630231bfb2ded406a5d2d70aefe
BhavinPadiya <143876932+BhavinPadiya@users.noreply.github.com>
2025-08-30 13:32:04 +0530

    feat(background): enable admin panel controls for background color and image

--- inc/classes/class-ritestay.php
+++ inc/classes/class-ritestay.php
@@ -25,19 +25,24 @@ class RITESTAY
     {
         //actions and filters
         add_action('after_setup_theme', [$this, 'setup_theme']);
-        
     }
 
     public function setup_theme()
     {
         add_theme_support('title-tag');
 
-	add_theme_support( 'custom-logo', [
-        'header-text'          => array( 'site-title', 'site-description' ),
-        'height'               => 100,
-		'width'                => 400,
-		'flex-height'          => true,
-		'flex-width'           => true,
-    ]);
+        add_theme_support('custom-logo', [
+            'header-text'          => array('site-title', 'site-description'),
+            'height'               => 100,
+            'width'                => 400,
+            'flex-height'          => true,
+            'flex-width'           => true,
+        ]);
+
+        $args = array(
+            'default-color' => '#fff',
+            'default-image' => '',
+        );
+        add_theme_support('custom-background', $args);
     }
 }
