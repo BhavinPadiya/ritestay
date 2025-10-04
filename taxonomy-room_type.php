8b973d57f3d83ceccd925cb7e2cd50bc3c6db904
BhavinPadiya <143876932+BhavinPadiya@users.noreply.github.com>
2025-10-04 17:24:18 +0530

    fix: improve HTML structure for room type taxonomy template and enhance image handling

--- taxonomy-room_type.php
+++ taxonomy-room_type.php
@@ -1,16 +1,22 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <?php 
+    <?php
     $term = get_queried_object();
-echo '<h1>Rooms: ' . esc_html( $term->name ) . '</h1>';
+    echo '<h1>Rooms: ' . esc_html($term->name) . '</h1>';
     ?>
     <div class="row">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
                 <div class="col-md-4">
                     <div class="card">
-                        <?php the_post_thumbnail('medium', array('class' => 'img-fluid')); ?>
+                        <a href="<?php the_permalink(); ?>" class="text-decoration-none text-dark">
+                            <?php if (has_post_thumbnail()) : ?>
+                                <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'room-card-thumb'); ?>" class="card-img-top" alt="<?php the_title_attribute(); ?>">
+                            <?php else : ?>
+                                <img src="https://via.placeholder.com/400x300.png?text=Room+Image+Placeholder" class="card-img-top" alt="Placeholder Room Image">
+                            <?php endif; ?>
+                        </a>
                         <div class="card-body">
                             <h5 class="card-title"><?php the_title(); ?></h5>
                             <?php the_content(); ?>
